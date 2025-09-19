f9f7b33e2be091c76c945f1f241a1d37c30677a2
Axel Bédu <axel.bedu.pro@gmail.com>
2025-09-19 09:14:26 +0200

    ❗Branche Axel

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"LvJXHJAm+8BCxDSGJxYX3vPSdv2Yv5Tf+EKUD72tQ6c=\"\n}"
